09d024de26d394d7bbb1c0f6e4aada92ff794dfe
Shubham Raut <rautshubham280@gmail.com>
2023-12-23 02:45:51 +0530

    start

---
+++ app/src/main/java/com/shubham/backspace/LoginAcitivity.java
@@ -0,0 +1,14 @@
+package com.shubham.backspace;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginAcitivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_acitivity);
+    }
+}
